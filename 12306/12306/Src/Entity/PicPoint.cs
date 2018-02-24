d11b1b8ad070a7d4b8e015ac59512e0762c6ab91
skw <281431280@qq.com>
2018-02-24 16:46:04 +0800

    12306

---
+++ 12306/12306/Src/Entity/PicPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace _12306
+{
+    class PicPoint
+    {
+        /// <summary>
+        /// 点X坐标
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// 点Y坐标
+        /// </summary>
+        public int Y { get; set; }
+    }
+
+}
